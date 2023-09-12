4807b7a1bcb0014c2543409db42d75070978d1da
Olatbossun <boyxdik@gmail.com>
2023-09-12 15:59:20 +0100

    admin page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { TypeOrmConfig } from '../typeorm.config';
 import { ProductsService } from './models/products.service';
 import { Product } from './models/product.entity';
+import { AdminModule } from './admin/admin.module';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeOrmConfig),
     TypeOrmModule.forFeature([Product]),
+    AdminModule,
   ],
   controllers: [AppController, ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService],
 })
 export class AppModule {}
