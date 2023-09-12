cfc620d3934ac18afaa694c030ab4231bf94890d
Olatbossun <boyxdik@gmail.com>
2023-09-12 20:37:04 +0100

    User Authenticatiion

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,20 @@ import { TypeOrmConfig } from '../typeorm.config';
 import { ProductsService } from './models/products.service';
 import { Product } from './models/product.entity';
 import { AdminModule } from './admin/admin.module';
+import { UsersService } from './models/users.service';
+import { User } from './models/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeOrmConfig),
-    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forFeature([Product, User]),
     AdminModule,
+    AuthModule,
   ],
   controllers: [AppController, ProductsController],
-  providers: [ProductsService],
-  exports: [ProductsService],
+  providers: [ProductsService, UsersService],
+  exports: [ProductsService, UsersService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
 import { resolve } from 'path';
+import * as session from 'express-session'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +13,18 @@ async function bootstrap() {
   hbs.registerPartials(resolve('./src/views/layouts'));
   hbsUtils(hbs).registerWatchedPartials(resolve('./src/views/layouts'));
   app.setViewEngine('hbs');
+
+  app.use(
+    session({
+      secret: 'nest-book',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+  });
 
   await app.listen(3000);
 }
